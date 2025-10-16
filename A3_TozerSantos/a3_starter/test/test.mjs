11be08d7549e07406021fd33925fbe657ef5335e
Sebastian Santos <sebysantos2006@gmail.com>
2025-10-16 12:51:54 -0300

    Update test.mjs

--- A3_TozerSantos/a3_starter/test/test.mjs
+++ A3_TozerSantos/a3_starter/test/test.mjs
@@ -5,7 +5,7 @@ import { rebuild } from "../db/DatabaseBuilder.mjs";
 import { processRequest } from "../api/restaurantService.mjs";
 import { MenuItem } from "../entity/MenuItem.mjs";
 import * as Constants from "../utils/constants.mjs";
-import { getItemByID, itemExists } from "../db/menuItemAccessor.mjs";
+import { getAllItems, getItemByID, itemExists } from "../db/menuItemAccessor.mjs";
 let testItems;
 
 describe("RestaurantService Tests", function () {
@@ -27,8 +27,11 @@ describe("RestaurantService Tests", function () {
             let result = await processRequest("LIST");
             assert.equal(result.statusCode,200);
             assert.isNull(result.err);
+
+            // Verifying that array is correct length, and objects are of type MenuItem
+            assert.equal(result.data.length,Constants.NUM_MENUITEMS);
             assert.isTrue(
-                result.data[0] instanceof MenuItem,
+                result.data[0] instanceof MenuItem && result.data[Constants.NUM_MENUITEMS-1] instanceof MenuItem,
                 "items should be instances of MenuItem"
             );
         });
@@ -38,9 +41,7 @@ describe("RestaurantService Tests", function () {
             assert.isNull(result.err);
             assert.isTrue(result.data);
 
-            // Verifies a change was made in the database
-            // let validate = await processRequest("ADD",testItems.itemToAdd); assert.isNull(validate.data);
-
+            // Verifying that entity has been added.
             assert.isTrue(await itemExists(testItems.itemToAdd));
         });
         it("DELETE deletes an item, returns status 200, null error, and data true, if item exists.", async function () {
@@ -49,9 +50,7 @@ describe("RestaurantService Tests", function () {
             assert.isNull(result.err);
             assert.isTrue(result.data);
 
-            // Verifies a change was made in the database
-            // let validate = await processRequest("DELETE",testItems.itemToDelete); assert.isNull(validate.data);
-
+            // Verifying that entity has been deleted.
             assert.isFalse(await itemExists(testItems.itemToDelete));
         });
         it("UPDATE updates an item, returns status 200, null error, and data true, if item exists.", async function () {
@@ -60,7 +59,7 @@ describe("RestaurantService Tests", function () {
             assert.isNull(result.err);
             assert.isTrue(result.data);
 
-            // Verifies a change was made in the database
+            // Verifying that entity has been updated
             let item = await getItemByID(testItems.itemToUpdate.id);
             assert.isTrue(item.category === testItems.itemToUpdate.category);
             assert.isTrue(item.description === testItems.itemToUpdate.description);
@@ -104,68 +103,116 @@ describe("RestaurantService Tests", function () {
         });
         describe("Content is not an Object - expect: status 400, error message, and null data", async function() {
             it("Content is omitted: DELETE does not change database, and returns expected response", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("DELETE");
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+                
+                // Verifying that nothing has been deleted from the database.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
             it("Content is null: DELETE does not change database, and returns expected response", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("DELETE",null);
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that nothing has been deleted from the database.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
             it("Content is a string: DELETE does not change database, and returns expected response", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("DELETE","test");
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that nothing has been deleted from the database.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
             it("Content is a valid ID (number): DELETE does not change database, and returns expected response", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("DELETE",100);
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that nothing has been deleted from the database.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
         });
         describe("Content is an Object but not a MenuItem - expect: status 400, error message, and null data", async function() {
             it("ADD returns expected response", async function () {
-                let result = await processRequest("ADD",testItems.wrongTypeItem);
+                let result = await processRequest("ADD",testItems.badItem14);
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that nothing has been added to the database.
+                assert.isFalse(await itemExists(testItems.badItem14));
             });
             it("DELETE returns expected response", async function () {
-                let result = await processRequest("DELETE",testItems.wrongTypeItem);
+                let result = await processRequest("DELETE",testItems.badItem15);
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that the real entity has not been deleted from the database.
+                assert.isTrue(await itemExists(testItems.badItem15));
             });
             it("UPDATE returns expected response", async function () {
-                let result = await processRequest("UPDATE",testItems.wrongTypeItem);
+                let result = await processRequest("UPDATE",testItems.badItem16);
                 assert.equal(result.statusCode,400);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that the real entity has not been changed in the database. 
+                let item = await getItemByID(testItems.badItem16.id);
+                assert.isFalse(item.category === testItems.badItem16.category);
+                assert.isFalse(item.description === testItems.badItem16.description);
+                assert.isFalse(item.price === testItems.badItem16.price);
+                assert.isFalse(item.vegetarian === testItems.badItem16.vegetarian);
             });
         });
         describe("Content is a MenuItem, but the operation cannot be performed", async function() {
             it("ADD does not change database, returns status 409, error message, and null data, if item already exists", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("ADD",testItems.goodItem);
                 assert.equal(result.statusCode,409);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that the database has not changed.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
             it("DELETE does not change database, returns status 404, error message, and null data, if item does not exist", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("DELETE",testItems.badItem);
                 assert.equal(result.statusCode,404);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that the database has not changed.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
             it("UPDATE does not change database, returns status 404, error message, and null data, if item does not exist", async function () {
+                let oldItems = await getAllItems();
                 let result = await processRequest("UPDATE",testItems.badItem);
                 assert.equal(result.statusCode,404);
                 assert.isNotNull(result.err);
                 assert.isNull(result.data);
+
+                // Verifying that the database has not changed.
+                let newItems = await getAllItems();
+                assert.equal(oldItems.length, newItems.length)
             });
         });
     });
@@ -221,5 +268,26 @@ async function defineTestItems() {
             price: 0,
             vegetarian: false,
         },
+        badItem14: {
+            id: 999,
+            category: "DES",
+            description: "apple crisp",
+            price: 12,
+            vegetarian: true,
+        },
+        badItem15: {
+            id: 201,
+            category: "ENT",
+            description: "black cod",
+            price: 21,
+            vegetarian: false,
+        },
+        badItem16: {
+            id: 301,
+            category: "DES",
+            description: "wrong",
+            price: -1,
+            vegetarian: false,
+        }
     };
 }
